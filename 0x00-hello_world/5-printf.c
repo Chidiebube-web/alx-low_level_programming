ca542611f59d83cd1571d6aecc8624f94d1a2470
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-10 09:30:23 -0800

    Task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Prints text withou error
+ *
+ *Return: 0 without errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
