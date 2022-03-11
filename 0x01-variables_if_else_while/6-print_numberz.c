3160de4410c35f2c6d4a7794242e1db9988de339
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-11 15:41:38 -0800

    Task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This gives an output
+ *
+ * Return: 0 Successful
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
