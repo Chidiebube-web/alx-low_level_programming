0661ff10c21bc6885e9214555804a9e8d33dfd4b
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-11 14:43:19 -0800

    Task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -This prints out code
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+		printf("%i", n);
+
+	putchar('\n');
+	return (0);
+}
