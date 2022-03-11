ab9e109539b07e07c0db8e3ffd4e0eaff3b71528
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-11 14:27:05 -0800

    Task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - This prints out code
+ *
+ * Return: 0 if Successful
+ */
+int main(void)
+{
+	char d;
+
+	d = 'a';
+
+	while (d <= 'z')
+	{
+		if ((d != 'e') && (d != 'q'))
+		{
+			putchar(c);
+			c++;
+		}
+		else
+			c++
+	}
+	putchar('\n');
+	return (0);
+}
