e937aa0071910ac8099d7c621eeda55a9a18d77e
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-11 14:06:39 -0800

    Task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This outputs code
+ *
+ * Return: 0 If successful
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
