4a587783e7894e044280907b5c467f6e2483f290
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 16:33:48 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - This function adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: The added result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
