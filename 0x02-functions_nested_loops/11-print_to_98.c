a430dcf4a7fda495ad8eebf346029bbbc4387427
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 16:58:50 -0700

    Task 11 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - This function prints all natural numbers from "n" to "98"
+ *
+ * @n: integer
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
+}
