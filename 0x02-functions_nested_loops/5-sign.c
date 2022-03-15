caab42f6876339ad4fc1880815d3ec20b506cb13
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 14:54:00 -0700

    Task 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks sign
+ * @n: The signs
+ * Return: returns different outputs
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
