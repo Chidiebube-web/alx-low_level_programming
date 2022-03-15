1f73063a5a1f31f29076fa195be2a03b46d85f70
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 10:05:42 -0700

    Task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the letters of the alphabets in small letters
+ */
+void print_alphabet(void)
+{
+	for (sl = 'a'; sl <= 'z'; sl++)
+		putchar (sl);
+		putchar (\n);
+}
