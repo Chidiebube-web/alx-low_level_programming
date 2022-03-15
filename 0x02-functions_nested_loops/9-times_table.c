2084d6f35466c629eb59a71a773d25079e592a95
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 16:25:35 -0700

    Task 9 commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - This prints the multiplication table of 9
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(48);
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			_putchar(44);
+			_putchar(32);
+			if (z <= 9)
+			{
+				_putchar(32);
+				_putchar(z + 48);
+			}
+			else
+			{
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
