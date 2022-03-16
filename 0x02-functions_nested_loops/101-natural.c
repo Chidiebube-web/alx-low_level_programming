85df7968e47f7f16fea11309e0b93a2a2cc4a456
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 17:42:15 -0700

    Task 13 commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints a set of natural numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
