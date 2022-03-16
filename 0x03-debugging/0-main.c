bedaf26d2a4161f1549eb904a502386e9aa1a071
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-16 16:08:23 -0700

    Debuggint tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints the different numbers at different occurrences
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
