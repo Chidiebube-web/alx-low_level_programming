9968ed03596d10fbf801929b08439d635f2aac50
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-15 20:21:16 -0700

    Task commot

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -36,15 +36,15 @@ int main(void)
 			fn2 = (n11 + n22);
 			fn = n1 + n2 + (fn2 / 1000000000);
 			printf(", %ld", fn);
-			printf"%ld", fn2 % 1000000000);
+			printf("%ld", fn2 % 1000000000);
 			n1 = n2;
 			n11 = n22;
 			n2 = fn;
-			n22 = (fn2 % 1000000000)
+			n22 = (fn2 % 1000000000);
 		}
-	}
-	if (((n1 + n2) < 0) && boolean == 1
+		if (((n1 + n2) < 0) && boolean == 1)
 			boolean = 0;
+	}
+	printf("\n");
+	return (0);
 }
-printf("\n");
-return (0);
