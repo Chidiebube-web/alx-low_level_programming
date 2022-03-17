e5d86a048b2471964acbac80d9d2e2342d4170b5
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-17 16:08:57 -0700

    Task 8 commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - This function prints a square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
