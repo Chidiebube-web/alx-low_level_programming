e84ed1afcaf9857b74b01d5f76971cdc41bdddcf
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-17 14:29:26 -0700

    task 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function prints out numbers 0 to 10
+ * and ends with a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
