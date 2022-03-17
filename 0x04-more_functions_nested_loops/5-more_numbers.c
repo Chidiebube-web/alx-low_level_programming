508e79389227a482f30e856f9b65f7305374f6b4
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-17 15:19:38 -0700

    Task 5 commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (ch >= 10)
-			_putchar((ch / 10) + 48);
-		_putchar((ch % 10) + 48);
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
 	}
 	_putchar('\n');
 }
