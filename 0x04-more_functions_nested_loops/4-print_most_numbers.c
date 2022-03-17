60021b2c069fb157bc9a7c802af2a0f404384d55
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-17 14:57:54 -0700

    Task 4 commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - This prints numbers from 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
