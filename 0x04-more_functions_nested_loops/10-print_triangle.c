0db48d37dfbdf24fe27491c67887292488b0e0cc
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-17 16:46:24 -0700

    Task 10 commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_triangle - This prints a triangle followed by a new line
+ * @size: number of lines of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
