b9fa1dd07ba343ef7c2118cfe354c90a7a4890bc
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-21 13:38:18 -0700

    Task 5 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - This prints the reverse of a string
+ * @s: String
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
+}
