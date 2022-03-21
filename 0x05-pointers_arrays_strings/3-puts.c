f5d0571b7fd3b31b1202a90eed7accf1b30d90a0
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-03-21 12:56:40 -0700

    Task 3 commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - This prints a string
+ * @str: String
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 1 on success
+ * else -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
