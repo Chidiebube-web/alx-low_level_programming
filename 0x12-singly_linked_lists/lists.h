70ad7a1602c401bb630471bfe023c5f937b8cd51
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-04-21 13:09:37 +0000

    Changes made

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
 
+/**
+ * struct list_s - singly linked lists
+ * @str: string
+ * @len: length of string
+ * @next: next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 
 #endif
