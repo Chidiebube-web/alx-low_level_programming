0090c73158266067903f1a2d8923320ba3823ed5
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-07-01 12:01:21 +0000

    header file updated

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,4 +40,8 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
+void replace_value(hash_node_t **ht, const char *key, const char *value);
+int check_key(hash_node_t *ht, const char *key);
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
+
 #endif
