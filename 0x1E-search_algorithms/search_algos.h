c18936772298bfc3783e806dbcb6bec7e5ae3c94
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-10-05 21:32:18 +0000

    header file updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,8 @@
 #include <unistd.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+void array_print(int *array, size_t lo, size_t hi);
 
 #endif
