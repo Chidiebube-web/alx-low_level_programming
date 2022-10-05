cbe0354781089756424a186b736a6610e2bb1056
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-10-05 21:59:58 +0000

    header file updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
 int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, in`t value);
 
 #endif
